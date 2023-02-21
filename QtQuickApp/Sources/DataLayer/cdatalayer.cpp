f5b94ffa2a8d26a229f01616ca6e47bce20959e2
Dat Doan <42245488+SmileDragonfly@users.noreply.github.com>
2023-02-21 22:54:01 +0700

    - Add get json value from data

--- QtQuickApp/Sources/DataLayer/cdatalayer.cpp
+++ QtQuickApp/Sources/DataLayer/cdatalayer.cpp
@@ -1,5 +1,8 @@
 #include "cdatalayer.h"
 #include <QDebug>
+#include <QJsonDocument>
+#include <QJsonArray>
+#include <QJsonObject>
 
 CDataLayer::CDataLayer(QObject *parent)
     : QObject{parent}
@@ -10,4 +13,8 @@ CDataLayer::CDataLayer(QObject *parent)
 void CDataLayer::slotSymbolPriceTicker(QString sPriceData)
 {
     qDebug() << "CDataLayer::slotSymbolPriceTicker: sPriceData= " << sPriceData;
+    QJsonDocument jsonDoc = QJsonDocument::fromJson(sPriceData.toUtf8());
+    QJsonArray jsonArr = jsonDoc.array();
+    qDebug() << jsonArr[0].toObject()["symbol"];
+    qDebug() << jsonArr[0].toObject()["price"];
 }
